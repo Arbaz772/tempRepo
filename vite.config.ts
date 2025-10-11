402e6bd2cac47ac285d7613f37614deab6184ca1
SM Arbaz Alam <smarbaz.alam@dell.com>
2025-10-11 19:31:31 +0530

    fixed446

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
   root: path.resolve(import.meta.dirname, "client"),
   build: {
     outDir: path.resolve(import.meta.dirname, "server/public"),
+	console.log("Vite build output directory:", path.resolve(import.meta.dirname, "server/public"));
     emptyOutDir: true,
   },
   server: {
