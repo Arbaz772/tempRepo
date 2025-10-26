b977d9b437686c5d806d7cc76fc5546390eebc85
SM Arbaz Alam <smarbaz.alam@dell.com>
2025-10-26 19:52:51 +0530

    app icon

--- client/src/keycloak.tsx
+++ client/src/keycloak.tsx
@@ -1,9 +1,14 @@
 import Keycloak from "keycloak-js";
 
+
 const keycloak = new Keycloak({
-  url: "https://keycloak.skailinker.org",
-  realm: "syed-arbaz-org-realm",
-  clientId: "skailinker-clientId",
+    url: 'https://keycloak.skailinker.org/auth',
+    realm: 'syed-arbaz-org-realm', // Use your exact realm ID/name
+    clientId: 'skailinker-clientId',
+    // NO secret or credentials property here
 });
 
-export default keycloak;
+keycloak.init({
+    onLoad: 'login-required', // or 'check-sso'
+    pkceMethod: 'S256', // Crucial for PKCE security
+});
