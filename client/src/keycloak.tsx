70f53c36c624a7e9c7049bad9df6d080014b9b9d
SM Arbaz Alam <smarbaz.alam@dell.com>
2025-10-26 17:52:25 +0530

    footer

--- client/src/keycloak.tsx
+++ client/src/keycloak.tsx
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "http://keycloak.skailinker.org/auth",
+  url: "https://keycloak.skailinker.org/auth",
   realm: "syed-arbaz-org-realm",
   clientId: "skailinker-clientId",
 });
